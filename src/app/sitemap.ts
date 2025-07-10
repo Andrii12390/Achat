febcb7fa7d1e6733a8e5190a32c1d884356a2642
Andrii <pasiura.andriy@lll.kpi.ua>
2025-07-10 13:35:02 +0300

    feat(seo): add sitemap.xml and robots.txt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+
+async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: BASE_URL,
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/register`,
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/verification`,
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+  ];
+}
+
+export default sitemap;
